c64b6f4d7e360e19eeef8fb768c067ad6df21a6e
Carlos Jimenez <carlosmjimenezg@gmail.com>
2025-01-20 20:06:47 -0400

    Update nav

--- src/components/menu/navbar.tsx
+++ src/components/menu/navbar.tsx
@@ -3,17 +3,15 @@ export default function Navbar() {
   return (
     <div>
       <header className="bg-[#8A4FFF] px-6 py-4 flex justify-between items-center">
-        <h1 className="text-white text-xl font-bold">SISTEMAS EXPERTOS</h1>
+        <Link href={"/home"}>
+          <h1 className="text-white text-xl font-bold">SISTEMAS EXPERTOS</h1>
+        </Link>
         <nav className="space-x-4 flex">
           <Link href={"/home"}>
-            <p className="text-white hover:text-white/90">
-              Inicio
-            </p>
+            <p className="text-white hover:text-white/90">Inicio</p>
           </Link>
           <Link href={"/home/chat"}>
-            <p className="text-white hover:text-white/90">
-              Chat
-            </p>
+            <p className="text-white hover:text-white/90">Chat</p>
           </Link>
         </nav>
       </header>
